c820be48440628dc0749a3c7dc71cc1ac6bf6e45
Krzysztof Langner <klangner@gmail.com>
2013-09-11 21:30:05 +0200

    First version with server integration

--- src/puzzle.py
+++ src/puzzle.py
@@ -3,10 +3,16 @@ Created on 08-09-2013
 
 @author: Krzysztof Langner
 '''
+from cStringIO import StringIO
+from pkg_resources import resource_string #@UnresolvedImport
+from webob.response import Response
 from xblock.core import XBlock, Scope
 from xblock.fields import String, Integer
 from xblock.fragment import Fragment
-from pkg_resources import resource_string #@UnresolvedImport
+import Image
+import cStringIO
+import urllib
+#from webob import Response
 
 
 class PuzzleBlock(XBlock):
@@ -14,9 +20,13 @@ class PuzzleBlock(XBlock):
     An XBlock providing puzzle activity based on given image
     """
 
-    image_path = String(help="Image path", default=None, scope=Scope.content)
-    cols = Integer(help="Numer of columns", default=4, scope=Scope.content)
-    rows = Integer(help="Number of rows", default=4, scope=Scope.content)
+    imageURL = String(help="Image URL", 
+                      default="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Puzzle-historical-map-1639.JPG/320px-Puzzle-historical-map-1639.JPG", 
+                      scope=Scope.content)
+    width = Integer(help="Puzzle width", default=640, scope=Scope.content)
+    height = Integer(help="Puzzle height", default=480, scope=Scope.content)
+    cols = Integer(help="Number of columns", default=6, scope=Scope.content)
+    rows = Integer(help="Number of rows", default=5, scope=Scope.content)
     
     def student_view(self, context):
         """
@@ -27,19 +37,39 @@ class PuzzleBlock(XBlock):
         to display.
         """
         html_str = resource_string(__name__, "static/puzzle/view.html")
-        frag = Fragment(unicode(html_str).format(self=self))
+        frag = Fragment(unicode(html_str))
         css_str = resource_string(__name__, "static/puzzle/puzzle.css")
-        frag.add_css(unicode(css_str))        
+        image_url = self.runtime.handler_url(self, 'image')
+        frag.add_css(unicode(css_str % (image_url, self.width, self.height)))       
+        frag.add_javascript_url('http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min.js') 
         js_str = resource_string(__name__, "static/puzzle/puzzle.js")
-        frag.add_javascript(unicode(js_str))
+        frag.add_javascript(unicode(js_str % (self.width, self.height, self.cols, self.rows)))
         frag.initialize_js('PuzzleBlock')        
         return frag    
     
     @XBlock.json_handler
-    def button_clicked(self, data):
+    def check(self, data):
         return {'response': 'ok'}    
-
-
+    
+    @XBlock.json_handler
+    def show_answer(self, data):
+        return {'response': 'ok'}    
+    
+    def image(self, request):
+        """Provide shuffled image."""
+        image_file = cStringIO.StringIO(urllib.urlopen(self.imageURL).read())
+        img = Image.open(image_file)
+        resized_img = img.resize((self.width, self.height), Image.ANTIALIAS)
+        tmp = StringIO()
+        resized_img.save(tmp, 'png')
+        tmp.seek(0)
+        response = Response()
+        response.body = tmp.getvalue()
+        response.headers['Content-Type'] = 'image/png'
+        tmp.close()
+        return response
+    
+    
 @staticmethod
 def workbench_scenarios():
     """A canned scenario for display in the workbench."""
@@ -47,7 +77,7 @@ def workbench_scenarios():
         ("Puzzle demo",
         """\
             <vertical>
-                <puzzle image="upload/puzzle.png" cols="4" rows="5"/>
+                <puzzle imageURL="upload/puzzle.png" cols="4" rows="5"/>
             </vertical>
          """)
     ]
--- src/static/puzzle/puzzle.js
+++ src/static/puzzle/puzzle.js
@@ -2,412 +2,127 @@ function PuzzleBlock(runtime, element) {
 
 	/** Run puzzle */
 	const testModel = {	
-		'Image':'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Puzzle-historical-map-1639.JPG/320px-Puzzle-historical-map-1639.JPG',
-		'width': 640,
-		'height': 480,
-		'cols':4,
-		'rows':4
+		width: %d,
+		height: %d,
+		columns: %d,
+		rows: %d
 	}
 	
-	createPuzzle($(element).find(".puzzle-container:first"), testModel);
-	connectButtonHandlers();
+	var presenter = new PuzzlePresenter(testModel, $(element).find(".puzzle-module:first"));
+	presenter.refresh();
+	connectButtonHandlers($(element).find(".puzzle-buttons:first"), presenter);
 	
-
-	/** 
-	 * Create puzzle  
+	/**
+	 * Presenter responsible for logic of the puzzle module
 	 */
-	function createPuzzle(puzzleView, puzzleModel) {
-
-	    const view = puzzleView;
-	    const model = puzzleModel;
-
-	    var board = []; // Array that will hold the 2-dimensional representation of the board.
-	    var indexBoard = [];
-	
-	    var animation = false;
-	    var clickNumber = 0; //Check if this is first or second click
-	
-	    //Keep data from first click
-	    var PieceOld;
-	    var PiecePos;
-	    var PiecePos2;
-	
-	    var puzzleWidth = 0;
-	    var puzzleOuterWidth = 0;
-	    var puzzleHeight = 0;
-	    var puzzleOuterHeight = 0;
-	    var leftOffset = 0;
-	    var topOffset = 0;
-	    var puzzle = null;
-	    var mark;
-	
-	    loadImageAndInitialize();
-	    
-	    /** Load image and initialize puzzle */
-	    function loadImageAndInitialize() {
-	    	
-	        var imageNode = view.find( "img:first" );
-        	imageNode.load(function() {
-                initPuzzle(model.width, model.height, model.rows, model.cols, imageNode);
+	function PuzzlePresenter(model, view){
+		
+		const _model = validateModel(model);
+		const _view = initView(view, _model);
+		const pieceWidth = _model.width/_model.columns;
+		const pieceHeight = _model.height/_model.rows;
+		var _selectedPiece = null;
+		/** Contains current pieces order */
+		var _currentOrder = [];
+		
+		/**
+		 * Reload puzzle.
+		 */
+		this.refresh = function(){
+			removeAllPieces();
+			createAllPieces();
+		}
+		
+		/**
+		 * Return current order of pieces
+		 */
+		this.getPiecesOrder = function(){
+			return _currentOrder;
+		}
+		
+		/** Ensure that model has valid parameters */
+		function validateModel(model){
+			return {
+				width: model.width || 640,
+				height: model.height || 480,
+				columns: model.columns || 4,
+				rows: model.rows || 4
+			};
+		}
+		
+		function initView(view, model){
+			return view.css({width: model.width + "px", height: model.height + "px"});
+		}
+		
+		function removeAllPieces(){
+			_view.children().remove();
+			_selectedPiece = null;
+			_currentOrder = [];
+		}
+		
+		function createAllPieces(){
+			for(var i = 0; i < _model.rows; i++){
+				for(var j = 0; j < _model.columns; j++){
+					var piece = createPiece(i, j);
+					_view.append(piece);
+					_currentOrder.push(piece.order);
+				}
+			}
+		}
+		
+		function createPiece(row, column){
+			var piece = $(document.createElement("div"));
+			piece.order = column + row*_model.columns;
+            piece.addClass('puzzle-piece');
+            piece.css({
+            	left: pieceWidth*column + "px",
+                top: pieceHeight*row + "px",
+                width: pieceWidth + 'px',
+                height: pieceHeight + 'px',
+                backgroundPosition: (column*-pieceWidth) + "px " + (row*-pieceHeight) + "px"
             });
-	        imageNode.attr('src', model.Image);
-	        imageNode.attr('height', model.height);
-	        imageNode.attr('width', model.width);
-	    };
+            piece.click(function(){pieceClicked(piece);});
+            return piece;
+		}
+		
+		function pieceClicked(piece){
+			if(existy(_selectedPiece)){
+				_selectedPiece.removeClass('puzzle-piece-selected');
+				if(_selectedPiece != piece) replacePiecesPosition(_selectedPiece, piece);
+				_selectedPiece = null;
+			}
+			else{
+				_selectedPiece = piece;
+				_selectedPiece.addClass('puzzle-piece-selected');
+			}
+		}
+		
+		function replacePiecesPosition(piece1, piece2){
+			var left1 = piece1.css('left');
+			var top1 = piece1.css('top');
+			piece1.css({left:piece2.css('left'), top:piece2.css('top')});
+			piece2.css({left:left1, top:top1});
+			swap(_currentOrder, piece1.order, piece2.order);
+		}
 
-	    /** Initialize puzzle */
-	    function initPuzzle(width, height, rows, columns, imageNode) {
-	        var outerDistances = getOuterDistances();
-	        var markDimensions = getMarkDimensions();
-	        var containerWidth = width - outerDistances.container.horizontal;
-	        var containerHeight = height - outerDistances.container.vertical;
-	
-	        puzzleWidth =  parseInt(containerWidth / columns - outerDistances.puzzle.horizontal, 10);
-	        puzzleOuterWidth = puzzleWidth + outerDistances.puzzle.horizontal;
-	        puzzleHeight = parseInt(containerHeight / rows - outerDistances.puzzle.vertical, 10);
-	        puzzleOuterHeight = puzzleHeight + outerDistances.puzzle.vertical;
-	
-	        topOffset = outerDistances.container.paddingTop;
-	        leftOffset = outerDistances.container.paddingLeft;
-	
-	        var markHorizontalOffset = (puzzleOuterWidth - markDimensions.width) / 2;
-	        var markVerticalOffset = (puzzleOuterHeight - markDimensions.height) / 2;
-	
-	        for (var row = 0; row < rows; row++) {
-	            board[row] = [];
-	            indexBoard[row] = [];
-	
-	            for (var col = 0; col < columns; col++) {
-	                mark = $(document.createElement('div'));
-	                mark.addClass('mark');
-	                mark.css({
-	                    top: ((puzzleHeight * row + markVerticalOffset) + "px"),
-	                    left: ((puzzleWidth * col + markHorizontalOffset) + "px")
-	                });
-	                mark.attr("position", row + "-" + col);
-	                indexBoard[row][col] = mark;
-	                view.append(mark);
-	
-	                puzzle = $(document.createElement("div"));
-	                puzzle.addClass('puzzle-piece');
-	                puzzle.css({
-	                    backgroundImage: "url( '" + imageNode.attr("src") + "' )",
-	                    backgroundSize: width + "px " + height + "px" ,
-	                    backgroundRepeat: "no-repeat",
-	                    backgroundPosition: (
-	                        (col * -puzzleWidth) + "px " +
-	                            (row * -puzzleHeight) + "px"
-	                        ),
-	                    top: ((puzzleOuterHeight * row + topOffset) + "px"),
-	                    left: ((puzzleOuterWidth * col + leftOffset) + "px"),
-	                    width: puzzleWidth + 'px',
-	                    height: puzzleHeight + 'px'
-	                });
-	
-	                puzzle.attr("href", "javascript:void( 0 );").click(clickHandler);
-	                puzzle.attr("position", row + "-" + col);
-	                board[row][col] = puzzle;
-	                view.append(puzzle);
-	            }
-	        }
-	
-	        view.css({
-	            width: (puzzleOuterWidth * columns) + 'px',
-	            height: (puzzleOuterHeight * rows) + 'px'
-	        });
-	
-	        addBorderClasses();
-	        shuffle();
-	    }
-	
-	    function getOuterDistances() {
-	        var containerDimensions = getElementDimensions(view);
-	        var containerDistances = calculateOuterDistance(containerDimensions);
-	
-	        var puzzle = $(document.createElement("div"));
-	        puzzle.addClass('puzzle-piece');
-	        view.append(puzzle);
-	        var puzzleDimensions = getElementDimensions(puzzle);
-	        var puzzleDistances = calculateOuterDistance(puzzleDimensions);
-	        puzzle.remove();
-	
-	        return {
-	            container: containerDistances,
-	            puzzle: puzzleDistances
-	        };
-	    }
-	
-	    function getElementDimensions(element) {
-	        return {
-	            border:{
-	                top:parseInt(element.css('border-top-width'), 10),
-	                bottom:parseInt(element.css('border-bottom-width'), 10),
-	                left:parseInt(element.css('border-left-width'), 10),
-	                right:parseInt(element.css('border-right-width'), 10)
-	            },
-	            margin:{
-	                top:parseInt(element.css('margin-top'), 10),
-	                bottom:parseInt(element.css('margin-bottom'), 10),
-	                left:parseInt(element.css('margin-left'), 10),
-	                right:parseInt(element.css('margin-right'), 10)
-	            },
-	            padding:{
-	                top:parseInt(element.css('padding-top'), 10),
-	                bottom:parseInt(element.css('padding-bottom'), 10),
-	                left:parseInt(element.css('padding-left'), 10),
-	                right:parseInt(element.css('padding-right'), 10)
-	            }
-	        };
-	    }
-	
-	    function calculateOuterDistance(elementDimensions) {
-	        var top = elementDimensions.border.top;
-	        top += elementDimensions.margin.top;
-	        top += elementDimensions.padding.top;
-	
-	        var bottom = elementDimensions.border.bottom;
-	        bottom += elementDimensions.margin.bottom;
-	        bottom += elementDimensions.padding.bottom;
-	
-	        var left = elementDimensions.border.left;
-	        left += elementDimensions.margin.left;
-	        left += elementDimensions.padding.left;
-	
-	        var right = elementDimensions.border.right;
-	        right += elementDimensions.margin.right;
-	        right += elementDimensions.padding.right;
-	
-	        return {
-	            vertical : top + bottom,
-	            horizontal : left + right,
-	            top: top,
-	            bottom: bottom,
-	            left: left,
-	            right: right,
-	            paddingLeft: elementDimensions.padding.left,
-	            paddingTop: elementDimensions.padding.top
-	        };
-	    }
-	
-	    function getMarkDimensions() {
-	        var tempMark = $(document.createElement('div'));
-	        $(tempMark).addClass('mark').addClass('correct');
-	        view.append(tempMark);
-	
-	        var markWidth = $(tempMark).width();
-	        var markHeight = $(tempMark).height();
-	
-	        $(tempMark).remove();
-	
-	        return {
-	            width: markWidth,
-	            height: markHeight
-	        };
-	    }
-	
-	    function addBorderClasses() {
-	        for (var h = 0; h < model.columns; h++) {
-	            $(board[0][h]).addClass('top');
-	            $(board[model.rows - 1][h]).addClass('bottom');
-	        }
-	
-	        for (var v = 0; v < model.rows; v++) {
-	            $(board[v][0]).addClass('left');
-	            $(board[v][model.columns - 1]).addClass('right');
-	        }
-	    }
-	
-	    /**
-	     * Fisher-Yates Shuffle algorithm: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-	     * Original algorithm is based on flatt, one-dimension array. For our purposes (working on two-dimension arrays)
-	     * firstly we have to flatten the structure.
-	     *
-	     * Additionally Knuth allows items to be shuffled multiple times - in our case each puzzle (array element) has to be
-	     * shuffled once, but the whole procedure should be repeated at least twice.
-	     */
-	
-	    function shuffleArray(array) {
-	        var flatArray = [],
-	            shuffleSequence = [],
-	            row, column, counter, index;
-	
-	        for (row = 0; row < array.length; row++) {
-	            for (column = 0; column < array[row].length; column++) {
-	                flatArray.push({ row: row, column: column });
-	            }
-	        }
-	
-	        counter = flatArray.length - 1;
-	
-	        // While there are at least two elements in the array we generate next shuffle sequence. If array has only one
-	        // element we end the sequence (there is no sense in shuffling puzzle in place).
-	        while (counter >= 2) {
-	            index = (Math.random() * counter) | 0;
-	
-	            shuffleSequence.push({
-	                row: { from: flatArray[counter].row, to: flatArray[index].row },
-	                column: { from: flatArray[counter].column, to: flatArray[index].column }
-	            });
-	
-	            flatArray.splice(index, 1);
-	            flatArray.splice(-1, 1);
-	
-	            counter -= 2;
-	        }
-	
-	        return shuffleSequence;
-	    };
-	
-	    function shuffle() {
-	        var i, iteration,
-	            shuffleSequence, shuffle,
-	            $firstPiece, $secondPiece;
-	
-	        animation = false; // Shuffling should be without animation
-	
-	        for (iteration = 0; iteration < 3; iteration++) {
-	            shuffleSequence = shuffleArray(board);
-	
-	            for (i = 0; i < shuffleSequence.length; i++) {
-	                shuffle = shuffleSequence[i];
-	
-	                $firstPiece = board[shuffle.row.from][shuffle.column.from];
-	                $firstPiece.trigger({
-	                    type: "click",
-	                    triggered: true
-	                });
-	
-	                $secondPiece = board[shuffle.row.to][shuffle.column.to];
-	                $secondPiece.trigger({
-	                    type: "click",
-	                    triggered: true
-	                });
-	            }
-	        }
-	
-	        animation = true;
-	    }
-	
-	    function elementHasClasses(element) {
-	        element = $(element);
-	
-	        return {
-	            top: element.hasClass('top'),
-	            bottom: element.hasClass('bottom'),
-	            left: element.hasClass('left'),
-	            right: element.hasClass('right')
-	        };
-	    }
-	
-	    function removeBorderClasses(element) {
-	        $(element).removeClass('top').removeClass('bottom').removeClass('left').removeClass('right');
-	    }
-	
-	    function applyBorderClasses(element, classes) {
-	        for (var className in classes) {
-	            if (classes[className]) {
-	                $(element).addClass(className);
-	            }
-	        }
-	    }
-	
-	    function replaceBorderClasses(firstElement, secondElement) {
-	        var firstElementClasses = elementHasClasses(firstElement)
-	        var secondElementClasses = elementHasClasses(secondElement)
-	
-	        removeBorderClasses(firstElement);
-	        removeBorderClasses(secondElement);
-	
-	        applyBorderClasses(firstElement, secondElementClasses);
-	        applyBorderClasses(secondElement, firstElementClasses);
-	    }
-	
-	    function isSamePiece(piece1, piece2) {
-	        var piece1ID = $(piece1).attr('position'),
-	            piece2ID = $(piece2).attr('position');
-	
-	        return piece1ID == piece2ID;
-	    }
-	
-	    function clickHandler(event) {
-	
-	        var Piece = $(this);
-	        // Check to see if we are in the middle of an animation.
-	        if (clickNumber == 0) {
-	            clickNumber = 1;
-	            PieceOld = $(this);
-	            PieceOld.addClass('selected');
-	            PiecePos = {
-	                top: parseInt(Piece.css("top")),
-	                left: parseInt(Piece.css("left"))
-	            };
-	            PiecePos.row = Math.floor(((PiecePos.top - topOffset) / puzzleOuterHeight) + 0.5);
-	            PiecePos.col = Math.floor(((PiecePos.left - leftOffset) / puzzleOuterWidth) + 0.5);
-	        } else {
-	            clickNumber = 0;
-	            PiecePos2 = {
-	                top: parseInt(Piece.css("top")),
-	                left: parseInt(Piece.css("left"))
-	            };
-	            PiecePos2.row = Math.floor(((PiecePos2.top - topOffset) / puzzleOuterHeight) + 0.5);
-	            PiecePos2.col = Math.floor(((PiecePos2.left - leftOffset) / puzzleOuterWidth) + 0.5);
-	            PieceOld.removeClass('selected');
-	
-	            if (isSamePiece(PieceOld, Piece)) return;
-	
-	            board[PiecePos2.row][PiecePos2.col] = PieceOld;
-	            board[PiecePos.row][PiecePos.col] = Piece;
-	
-	            if (animation) {
-	                //Animate change of places
-	                board[PiecePos.row][PiecePos.col].animate({
-	                    left: ((puzzleOuterWidth * PiecePos.col + leftOffset) + "px"),
-	                    top: ((puzzleOuterHeight * PiecePos.row + topOffset) + "px")
-	                }, 200);
-	
-	                board[PiecePos2.row][PiecePos2.col].animate({
-	                    left: ((puzzleOuterWidth * PiecePos2.col + leftOffset) + "px"),
-	                    top: ((puzzleOuterHeight * PiecePos2.row + topOffset) + "px")
-	                }, 200);
-	            } else {
-	                board[PiecePos.row][PiecePos.col].css({
-	                    left: ((puzzleOuterWidth * PiecePos.col + leftOffset) + "px"),
-	                    top: ((puzzleOuterHeight * PiecePos.row + topOffset) + "px")
-	                });
-	                board[PiecePos2.row][PiecePos2.col].css({
-	                    left: ((puzzleOuterWidth * PiecePos2.col + leftOffset) + "px"),
-	                    top: ((puzzleOuterHeight * PiecePos2.row + topOffset) + "px")
-	                });
-	            }
-	
-	            replaceBorderClasses(board[PiecePos.row][PiecePos.col], board[PiecePos2.row][PiecePos2.col]);
-	        }
-	    }
-	
-	    function clearPieceCssStyle() {
-	        for (var rowIndex = 0; rowIndex < model.rows; rowIndex++) {
-	            for (var colIndex = 0; colIndex < model.columns; colIndex++) {
-	                indexBoard[rowIndex][colIndex].removeClass('wrong').removeClass('correct');
-	            }
-	        }
-	    }
-	
+		function swap(list, index1, index2){
+			var tmp = list[index1]; 
+			list[index1] = list[index2];
+			list[index2] = tmp;
+		}
 	}
-
-	/** 
-	 * Connect buttons to send result to the server 
-	 */
-	function connectButtonHandlers(){
-		$('.puzzleButton').click(function button_clicked() {
-			$.ajax({
-				type: "POST",
-		        url: runtime.handler_url('button_clicked'),
-		        data: JSON.stringify({watched: true}),
-		        success: function(result) {
-		        	console.log(result);
-		        }
-		    });
+	
+	
+	function connectButtonHandlers(button_panel, presenter){
+		var checkButton = $(button_panel.find("button")[0]);
+		checkButton.click(function() {
+			console.log("Order: " + presenter.getPiecesOrder());
 		});
 	}
-		
+	
+	/**
+	 * Utility functions
+	 */
+	function existy(x) { return x!= null};
+	function truthy(x) { return (x !== false) && existy(x)};
 }
